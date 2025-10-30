7d59ce1e471c109d4b037967345deb4d00028c6d
dhson <saozzang@gmail.com>
2025-10-30 22:08:14 +0900

    first commit

---
+++ src/main/java/com/example/mpa_login/MpaLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.mpa_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpaLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpaLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mpa_login/MpaLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mpa_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpaLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
